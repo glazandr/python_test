04bb30dbf31a217248f462963c7005930e0ce3de
glazandr <glazandr3412@yandex.ru>
2023-06-03 23:56:25 +0300

    change code in main file

--- main.py
+++ main.py
@@ -1,36 +1,55 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 import time
+import math
+import os
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def calc(x):
+    return math.log(abs(12 * math.sin(x)))
+
 
 try:
-    link = "http://suninjuly.github.io/registration1.html"
+    link = "http://suninjuly.github.io/explicit_wait2.html"
     browser = webdriver.Chrome()
     browser.get(link)
 
-    # Ваш код, который заполняет обязательные поля
-    input1 = browser.find_element(By.CLASS_NAME, "form-control.first[required]")
-    input1.send_keys("Ivan")
-    input2 = browser.find_element(By.CLASS_NAME, "form-control.second[required]")
-    input2.send_keys("Petrov")
-    input3 = browser.find_element(By.CLASS_NAME, "form-control.third[required]")
-    input3.send_keys("mail@mail.com")
+    # говорим Selenium проверять в течение 5 секунд, пока кнопка не станет кликабельной
+    price = WebDriverWait(browser, 12).until(
+        EC.text_to_be_present_in_element((By.ID, "price"), "$100")
+    )
+    button = browser.find_element(By.ID, "book")
+    button.click()
 
-    time.sleep(3)
+
+
+
+    num1 = browser.find_element(By.ID, "input_value").text
+    x = int(num1)
+    y = calc(x)
+    y = str(y)
+
+    input3 = browser.find_element(By.ID, "answer")
+    input3.send_keys(y)
+
     # Отправляем заполненную форму
-    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button = browser.find_element(By.ID, "solve")
     button.click()
 
     # Проверяем, что смогли зарегистрироваться
     # ждем загрузки страницы
-    time.sleep(1)
 
-    # находим элемент, содержащий текст
-    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
-    # записываем в переменную welcome_text текст из элемента welcome_text_elt
-    welcome_text = welcome_text_elt.text
-
-    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
-    assert "Congratulations! You have successfully registered!" == welcome_text
+    #
+    # # находим элемент, содержащий текст
+    # welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+    # # записываем в переменную welcome_text текст из элемента welcome_text_elt
+    # welcome_text = welcome_text_elt.text
+    #
+    # # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+    # assert "Congratulations! You have successfully registered!" == welcome_text
 
 finally:
     # ожидание чтобы визуально оценить результаты прохождения скрипта
